88fba8543ea5b458c4b210c0667161fb1a9fa6e6
Yanxzyx <sollxfim@gmail.com>
2026-08-22 02:39:58 +0800

    Add bypass_attack to init.py imports

--- attacks/init.py
+++ attacks/init.py
@@ -1,1 +1,1 @@
-from . import udp_flood, syn_flood, icmp_flood, tcp_flood, http_flood, slowloris, dns_amp, ntp_amp, memcached_amp
+from . import udp_flood, syn_flood, icmp_flood, tcp_flood, http_flood, slowloris, dns_amp, ntp_amp, memcached_amp, bypass_attack
